5c15ffdbad00286186ef6374b0d7a22aff247e55
Arjun Nair <nair.arjun89@gmail.com>
2018-05-01 20:42:09 -0400

    Total amount by customer

---
+++ src/main/scala/com/arnair/AmountByCustomer.scala
@@ -0,0 +1,27 @@
+package com.arnair
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+object AmountByCustomer {
+
+  def parse(line: String): (Int, Float) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
+  }
+
+  def main(args: Array[String]): Unit = {
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "FriendsByAge")
+
+    // Load each line of the source data into an RDD
+    val lines = sc.textFile("/home/arnair/Documents/udemy/SparkScala/customer-orders.csv")
+    val customerAmountRDD = lines.map(parse)
+    val results = customerAmountRDD.reduceByKey((x, y) => x+y).collect()
+    results.sorted.foreach(println(_))
+  }
+
+}
